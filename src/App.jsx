2cd5d2f145a81f9d277c6e0e775d0ce1d922ba9e
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2022-04-06 11:31:11 +0100

    gh-pages deployment fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,51 @@
-import React, {useEffect, useState} from 'react';
-import styles from './App.module.scss';
-import CardList from './components/CardList/CardList';
-import Navbar from './components/Navbar/Navbar';
-import {getBeers} from './services/beers.service';
-
+import React, { useEffect, useState } from "react";
+import styles from "./App.module.scss";
+import CardList from "./components/CardList/CardList";
+import Navbar from "./components/Navbar/Navbar";
+import { getBeers } from "./services/beers.service";
 
 const App = () => {
   const [beers, setBeers] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [checked, setChecked] = useState(false);
   const [checkedOne, setCheckedOne] = useState(false);
   const [checkedTwo, setCheckedTwo] = useState(false);
-  
+
   const updateApiBeers = async () => {
     const apiBeers = await getBeers();
     return setBeers(apiBeers);
-  }
-  
+  };
+
   useEffect(() => {
-    updateApiBeers(); 
+    updateApiBeers();
   }, []);
-  
+
   return (
     <div className={styles.parent}>
       <section className={styles.nav}>
-
-          <Navbar beers={beers} input={input} setInput={setInput} checked={checked} setChecked={setChecked} checkedOne={checkedOne} setCheckedOne={setCheckedOne} checkedTwo={checkedTwo} setCheckedTwo={setCheckedTwo} />
+        <Navbar
+          beers={beers}
+          input={input}
+          setInput={setInput}
+          checked={checked}
+          setChecked={setChecked}
+          checkedOne={checkedOne}
+          setCheckedOne={setCheckedOne}
+          checkedTwo={checkedTwo}
+          setCheckedTwo={setCheckedTwo}
+        />
       </section>
       <section className={styles.wrapper}>
-      
-       <CardList  beers={beers} input={input} checked={checked} checkedOne={checkedOne} checkedTwo={checkedTwo} />
+        <CardList
+          beers={beers}
+          input={input}
+          checked={checked}
+          checkedOne={checkedOne}
+          checkedTwo={checkedTwo}
+        />
       </section>
-       
     </div>
-  )
+  );
 };
 
 export default App;
-
-
-
-
-
-
-
-
